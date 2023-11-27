25d7eda87b6158136e2cdd9881f1a41431bdbcf9
varunpats <varunpatil2502@gmail.com>
2023-11-27 11:13:30 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDICkNlS_ho8ybJm7gaqFBRP3PVUCUNiRU",
+    authDomain: "netflixclone-6fdea.firebaseapp.com",
+    projectId: "netflixclone-6fdea",
+    storageBucket: "netflixclone-6fdea.appspot.com",
+    messagingSenderId: "243956141526",
+    appId: "1:243956141526:web:422a1ef5d37bc739a32877"
+};
