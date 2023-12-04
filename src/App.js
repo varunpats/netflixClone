90e04aa5f48a914f49b012d589cc72ace4389e82
varunpats <varunpatil2502@gmail.com>
2023-12-04 12:04:00 +0530

    Setup redux store and stored user details

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Loginscreen from './screens/Loginscreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { auth } from './firebase';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const subscirption = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
-
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }));
       } else {
-
+        dispatch(logout);
       }
     });
 
